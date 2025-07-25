7ece57e9735fe5d8a150c1c6d164f33800e10bde
arthur.gevorgyan <arthur.gevorgyan@servicetitan.com>
2025-07-25 18:14:32 +0400

    run_swarm.sh: add tcpclient serial connections for improved communication

--- Tools/autotest/run_swarm.sh
+++ Tools/autotest/run_swarm.sh
@@ -6,6 +6,7 @@ mkdir -p ~/ardupilot/{1,2}
   /home/gart/ardupilot/build/sitl/bin/arduplane \
       -S --model plane --speedup 5 \
       --serial0=udpclient:0.0.0.0:5000 \
+      --serial1=tcpclient:127.0.1:5761 \
       --sim-address=127.0.0.1 -I0 \
       --home 40.3117414,44.455211099999985,1294.86,0 \
       --sysid 1
@@ -15,6 +16,7 @@ mkdir -p ~/ardupilot/{1,2}
   /home/gart/ardupilot/build/sitl/bin/arduplane \
       -S --model plane --speedup 5 \
       --serial0=udpclient:0.0.0.0:6000 \
+      --serial1=tcpclient:127.0.1:5771 \
       --sim-address=127.0.0.1 -I1 \
       --home 40.3117414,44.455211099999985,1294.86,0 \
       --sysid 2
