aecb9fa5e03be16be49e2f27ba0f5f7169d23287
arthur.gevorgyan <arthur.gevorgyan@servicetitan.com>
2025-07-27 07:13:53 +0400

    run_warm_sim.sh: rename script and update serial connection ports

--- Tools/autotest/run_warm_sim.sh
+++ Tools/autotest/run_warm_sim.sh
@@ -6,7 +6,7 @@ mkdir -p ~/ardupilot/{1,2}
   /home/gart/ardupilot/build/sitl/bin/arduplane \
       -S --model plane --speedup 5 \
       --serial0=udpclient:0.0.0.0:5000 \
-      --serial1=udpclient:172.21.176.1:16550 \
+      --serial1=udpclient:172.21.176.1:14560 \ 
       --sim-address=127.0.0.1 -I0 \
       --home 40.3117414,44.455211099999985,1294.86,0 \
       --sysid 1
@@ -16,13 +16,17 @@ mkdir -p ~/ardupilot/{1,2}
   /home/gart/ardupilot/build/sitl/bin/arduplane \
       -S --model plane --speedup 5 \
       --serial0=udpclient:0.0.0.0:6000 \
-      --serial1=udpclient:172.21.176.1:16560 \
+      --serial1=udpclient:172.21.176.1:14570 \
       --sim-address=127.0.0.1 -I1 \
       --home 40.3117414,44.455211099999985,1294.86,0 \
       --sysid 2
 ) &
 (
-  mavlink-routerd -c /home/gart/ardupilot/Tools/autotest/swarm-mavlink-router.conf -v
+  mavlink-routerd -v \
+      --tcp-port 0 \
+      --endpoint 172.21.176.1:14550 \
+      127.0.0.1:5000 \
+      127.0.0.1:6000
 ) &
-
-wait            # keep script alive until everything exits
+    
+wait
