fd4adf8d5a2f565a96085c29d70e1bb95a836192
arthur.gevorgyan <arthur.gevorgyan@servicetitan.com>
2025-07-27 07:35:45 +0400

    run_swarm_sim.sh: update tcpclient serial connection address for local testing

--- Tools/autotest/run_swarm_sim.sh
+++ Tools/autotest/run_swarm_sim.sh
@@ -6,7 +6,7 @@ mkdir -p ~/ardupilot/{1,2}
   /home/gart/ardupilot/build/sitl/bin/arduplane \
       -S --model plane --speedup 5 \
       --serial0=udpclient:0.0.0.0:5000 \
-      --serial1=tcpclient:172.21.176.1:5760 \
+      --serial1=tcpclient:127.0.0.1:5760 \
       --sim-address=127.0.0.1 -I0 \
       --home 40.3117414,44.455211099999985,1294.86,0 \
       --sysid 1
