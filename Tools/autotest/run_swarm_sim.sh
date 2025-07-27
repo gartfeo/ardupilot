046aa6f2e7c1da67cb0a1db2f7f3682fa2553d1a
arthur.gevorgyan <arthur.gevorgyan@servicetitan.com>
2025-07-27 07:43:30 +0400

    run_swarm_sim.sh: swap serial connection protocols for improved functionality

--- Tools/autotest/run_swarm_sim.sh
+++ Tools/autotest/run_swarm_sim.sh
@@ -5,8 +5,8 @@ mkdir -p ~/ardupilot/{1,2}
   cd ~/ardupilot/1
   /home/gart/ardupilot/build/sitl/bin/arduplane \
       -S --model plane --speedup 5 \
-      --serial0=udpclient:0.0.0.0:5000 \
-      --serial1=tcp:127.21.176.1:5760:nowait \
+      --serial0=tcp:0.0.0.0:5760:nowait \
+      --serial1=udpclient:0.0.0.0:5000 \
       --sim-address=127.0.0.1 -I0 \
       --home 40.3117414,44.455211099999985,1294.86,0 \
       --sysid 1
@@ -15,8 +15,8 @@ mkdir -p ~/ardupilot/{1,2}
   cd ~/ardupilot/2
   /home/gart/ardupilot/build/sitl/bin/arduplane \
       -S --model plane --speedup 5 \
-      --serial0=udpclient:0.0.0.0:6000 \
-      --serial1=tcp:172.21.176.1:5770:nowait \
+      --serial0=tcp:0.0.0.0:5760:nowait \
+      --serial1=udpclient:0.0.0.0:6000 \
       --sim-address=127.0.0.1 -I1 \
       --home 40.3117414,44.455211099999985,1294.86,0 \
       --sysid 2
