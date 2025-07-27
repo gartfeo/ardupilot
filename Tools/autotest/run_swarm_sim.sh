83579e10fc3af7fabcec16bc972633c8f739d2e1
arthur.gevorgyan <arthur.gevorgyan@servicetitan.com>
2025-07-27 07:34:31 +0400

    run_swarm_sim.sh: update serial connections to tcpclient for improved compatibility

--- Tools/autotest/run_swarm_sim.sh
+++ Tools/autotest/run_swarm_sim.sh
@@ -6,7 +6,7 @@ mkdir -p ~/ardupilot/{1,2}
   /home/gart/ardupilot/build/sitl/bin/arduplane \
       -S --model plane --speedup 5 \
       --serial0=udpclient:0.0.0.0:5000 \
-      --serial1=udpclient:172.21.176.1:14560 \
+      --serial1=tcpclient:172.21.176.1:5760 \
       --sim-address=127.0.0.1 -I0 \
       --home 40.3117414,44.455211099999985,1294.86,0 \
       --sysid 1
@@ -16,7 +16,7 @@ mkdir -p ~/ardupilot/{1,2}
   /home/gart/ardupilot/build/sitl/bin/arduplane \
       -S --model plane --speedup 5 \
       --serial0=udpclient:0.0.0.0:6000 \
-      --serial1=udpclient:172.21.176.1:14570 \
+      --serial1=tcpclient:172.21.176.1:5770 \
       --sim-address=127.0.0.1 -I1 \
       --home 40.3117414,44.455211099999985,1294.86,0 \
       --sysid 2
