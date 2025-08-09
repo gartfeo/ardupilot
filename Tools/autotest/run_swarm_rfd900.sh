5259b339c068c611c943704a940605ed5f67ccda
arthur.gevorgyan <arthur.gevorgyan@servicetitan.com>
2025-08-09 08:52:06 +0400

    update serial configurations in swarm scripts and add new swarm simulation script

--- Tools/autotest/run_swarm_rfd900.sh
+++ Tools/autotest/run_swarm_rfd900.sh
@@ -6,7 +6,8 @@ mkdir -p ~/ardupilot/{1,2}
   /home/gart/ardupilot/build/sitl/bin/arduplane \
       -S --model plane --speedup 5 \
       --serial0=tcp:0.0.0.0:5660:nowait \
-      --serial1=uart:/dev/ttyUSB0:115200 \
+      --serial1=tcp:172.21.176.1:5762 \
+      --serial2=uart:/dev/ttyUSB0:115200 \
       --sim-address=127.0.0.1 -I0 \
       --home 40.3117414,44.455211099999985,1294.86,0 \
       --sysid 1
@@ -16,7 +17,8 @@ mkdir -p ~/ardupilot/{1,2}
   /home/gart/ardupilot/build/sitl/bin/arduplane \
       -S --model plane --speedup 5 \
       --serial0=tcp:0.0.0.0:5670:nowait \
-      --serial1=uart:/dev/ttyUSB1:115200 \
+      --serial1=tcp:172.21.176.1:5772 \
+      --serial2=uart:/dev/ttyUSB1:115200 \
       --sim-address=127.0.0.1 -I1 \
       --home 40.3117414,44.455211099999985,1294.86,0 \
       --sysid 2
